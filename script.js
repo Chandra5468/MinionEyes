3feba9e6797acbafcc6be7fc546f633aec0b2fad
Chandra5468 <agirchandra@gmail.com>
2022-03-04 16:40:49 +0530

    Descitption inside terminal

---
+++ script.js
@@ -0,0 +1,54 @@
+// Selecting the eye div
+
+let eye_ref = document.querySelectorAll(".eye")
+
+
+//Mouse move for devices with mouse and touchmove for touch screen
+
+let events = ["mousemove","touchmove"]
+
+// Check for touch screen
+function isTouchDevice(){
+    try{
+        document.createEvent("TouchEvent")
+        return true
+    }catch(e){
+        return false
+    }
+}
+
+// Same function for both events
+events.forEach((eventType)=>{
+
+    document.body.addEventListener(eventType,(event) =>{
+
+        eye_ref.forEach((eye)=>{
+            // GetBoundingClientRect() method
+            //returns the position relative to the Viewport
+
+            let eyeX = eye.getBoundingClientRect().left + eye.clientWidth /2;
+            let eyeY = eye.getBoundingClientRect().top + eye.clientHeight /2;
+
+            /* ClientX and ClientY return the position of clients cursor from top
+            left of the screen*/
+
+            var x = !isTouchDevice() ? event.clientX : event.touches[0].clientX;
+            var y = !isTouchDevice() ? event.clientY : event.touches[0].clientY;
+
+            /* Subtract x position of mouse from x position of eye
+            and y position of mouse from y position of eye
+            Use atan2(returns angle in radians) */
+
+
+            let radian = Math.atan2(x - eyeX, y - eyeY)
+
+            // Convert Radians to degrees
+
+            let rotationDegrees = radian * (180/ Math.PI) * -1 + 180;
+
+            // Rotate the eye
+            eye.style.transform = "rotate("+rotationDegrees+"deg)"
+            
+        })
+    })
+})
